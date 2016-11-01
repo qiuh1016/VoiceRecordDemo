7e026b4905652cce8df3771324afcac58587982c
qiuh1016 <qiuh1016@126.com>
2016-11-01 11:31:48 +0800

    Initial Commit

---
+++ VoiceRecordDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceRecordDemo
+//
+//  Created by qiuhong on 01/11/2016.
+//  Copyright © 2016 CETCME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
